9293b02e72f60a995cd800a427e49b877035125e
mohmmad <yazdanpanah.moh@gmail.com>
2020-03-23 19:16:19 +0430

    add section portfolio

---
+++ assets/js/coustom.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    
+    $(".img-to-show").hover(function () {
+        $(this).css("opacity", "0");
+    }, function () {
+        $(this).css("opacity", "1");
+    });
+
+
+});
