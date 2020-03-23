7d235a75a35c0f55e509041a561864437f94a4eb
mohmmad <yazdanpanah.moh@gmail.com>
2020-03-24 02:07:18 +0430

    create contact us and footer

--- assets/js/coustom.js
+++ assets/js/coustom.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+    // main menu code 
+    var top = 0;
+    $(window).on('scroll', function () {
+        $("#main-menu-id").toggleClass("hide", $(window).scrollTop() > top);
+        top = $(window).scrollTop();
+    });
 
     // portfolio code 
     $(".img-to-show").hover(function () {
@@ -7,5 +13,16 @@ $(document).ready(function () {
         $(this).css("opacity", "1");
     });
 
-
+    //counter-up code
+    // $('.number-counter').each(function () {
+    //         $(this).prop('Counter', 0).animate({
+    //             Counter: $(this).text()
+    //         }, {
+    //             duration: 5000,
+    //             easing: 'swing',
+    //             step: function (now) {
+    //                 $(this).text(Math.ceil(now));
+    //             }
+    //         });
+    //     });
 });
