61362594c7dcd75fee74a6a7721a8254f041d0ec
mohmmad <yazdanpanah.moh@gmail.com>
2020-03-27 19:58:47 +0430

    add mobile menu and write js code

--- assets/js/coustom.js
+++ assets/js/coustom.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
+    let countdone = true;
     // main menu code 
     var top = 0;
+    $("#toggle-menu-btn").click(function () {
+        console.log("show")
+        $(".mobile-nav-menu").addClass("show")
+    })
+    $("#close-menu-btn").click(function(){
+        console.log("unshow")
+        $(".mobile-nav-menu").removeClass("show");
+    })
     $(window).on('scroll', function () {
         $("#main-menu-id").toggleClass("hide", $(window).scrollTop() > top);
         top = $(window).scrollTop();
@@ -13,16 +22,42 @@ $(document).ready(function () {
         $(this).css("opacity", "1");
     });
 
-    //counter-up code
-    // $('.number-counter').each(function () {
-    //         $(this).prop('Counter', 0).animate({
-    //             Counter: $(this).text()
-    //         }, {
-    //             duration: 5000,
-    //             easing: 'swing',
-    //             step: function (now) {
-    //                 $(this).text(Math.ceil(now));
-    //             }
-    //         });
-    //     });
+
+    $(window).on("scroll", function () {
+        let windowScroll = $(window).scrollTop()
+        let counterUp = $(".section-counter-up").offset().top
+        if (windowScroll >= counterUp || counterUp <= windowScroll) {
+            if (countdone){
+                countNumber();
+            }
+            countdone = false
+            
+        }
+    })
+    function countNumber() {
+        $('.counter').each(function () {
+            var $this = $(this);
+            var countTo = $this.attr('data-count');
+
+            $({
+                countNum: $this.text()
+            }).animate({
+                    countNum: countTo
+                },
+
+                {
+
+                    duration: 2000,
+                    easing: 'linear',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function () {
+                        $this.text(this.countNum + " + ");
+                        //alert('finished');
+                    }
+
+                });
+        });
+    }
 });
